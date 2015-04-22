0bbb955020441b446720e2f8a2e4a577e2acd2e6
Andrew Joseph <andrew@aljweb.com>
2015-04-22 01:55:19 -0500

    update install script to back up user config files and replace with links to these
    Signed-off-by: Andrew Joseph <andrew@aljweb.com>

--- install.sh
+++ install.sh
@@ -5,18 +5,21 @@ for FILE in $(find . -name \.\* -type f  -print | sed 's/.\///g'); do
 	OLDSIZE=$(ls -lA $OLDFILE 2> /dev/null | cut -f5 -d' ')
 	SIZE=$(ls -lA $FILE | cut -f5 -d' ')
 	FILENAME=$(pwd)/$FILE
-	# echo $FILE $OLDFILE
-	# echo $SIZE $OLDSIZE
 	if [[ $SIZE != $OLDSIZE ]]; then
 		if [[ -f $OLDFILE ]]; then
-			mv $OLDFILE $OLDFILE.bak.$(date +%Y%m%d%H%M%S)
-			ln -s $FILENAME $OLDFILE
-			echo Replaced $FILE
-		else
-			read -p "$OLDFILE does not exist. Link this file? " -n 1 -r
-			echo    # (optional) move to a new line
+			read -p "$OLDFILE exists. Overwrite this file? " -n 1 -r
+			echo
 			if [[ $REPLY =~ ^[Yy]$ ]]; then
-				ln -s $FILENAME $OLDFILE
+				mv $OLDFILE $OLDFILE.bak.$(date +%Y%m%d%H%M%S)
+				# ln -s $FILENAME $OLDFILE
+				cp $FILENAME $OLDFILE
+				echo Updated $FILE
+			fi
+		else
+			read -p "$OLDFILE does not exist. Add this file? " -n 1 -r
+			echo
+			if [[ $REPLY =~ ^[Yy]$ ]]; then
+				cp $FILENAME $OLDFILE
 				echo Added $FILE
 			fi
 		fi
